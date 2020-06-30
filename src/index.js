3248fea611c4ed02a3463680bf2a4f635214b3a3
Lewi Samuel <arroba.lewi@gmail.com>
2020-06-30 08:42:03 -0300

    remove sendgrid key

--- src/index.js
+++ src/index.js
@@ -4,13 +4,11 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-// import Email from './pages/email';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path='/' exact component={App} />
-      {/* <Route path='/email' exact component={Email} /> */}
     </Switch>
   </BrowserRouter>,
   document.getElementById('root')
